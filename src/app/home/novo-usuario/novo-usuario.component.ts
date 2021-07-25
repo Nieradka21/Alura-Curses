a0bed89fb8ac4c265690895a3958cb798a1d50a5
Ivan Mateus (WRDZ) <ter.imateus@restoque.com.br>
2021-07-24 21:34:07 -0300

    gatitobook

--- src/app/home/novo-usuario/novo-usuario.component.ts
+++ src/app/home/novo-usuario/novo-usuario.component.ts
@@ -1,15 +1,42 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { minusculoValidator } from './minusculo.validator';
+import { NovoUsuario } from './novo-usuario';
+import { NovoUsuarioService } from './novo-usuario.service';
 
 @Component({
   selector: 'app-novo-usuario',
   templateUrl: './novo-usuario.component.html',
-  styleUrls: ['./novo-usuario.component.css']
+  styleUrls: ['./novo-usuario.component.css'],
 })
 export class NovoUsuarioComponent implements OnInit {
-
-  constructor() { }
+  novoUsuarioForm!: FormGroup;
+  constructor(private form: FormBuilder, private service: NovoUsuarioService) {}
 
   ngOnInit(): void {
+    this.novoUsuario();
   }
 
+  novoUsuario() {
+    this.novoUsuarioForm = this.form.group({
+      email: ['', [Validators.required, Validators.email]],
+      fullName: ['', [Validators.required, Validators.minLength(4)]],
+      userName: ['', [Validators.minLength(3), minusculoValidator]],
+      passWord: ['', [Validators.required, Validators.minLength(8)]],
+    });
+  }
+
+  cadastrar() {
+    if (this.novoUsuarioForm.valid) {
+      const novoUsuario = this.novoUsuarioForm.getRawValue() as NovoUsuario;
+      this.service.cadastraNovoUsuario(novoUsuario).subscribe(
+        () => {
+          console.log(novoUsuario);
+        },
+        (err) => {
+          console.log(err);
+        }
+      );
+    }
+  }
 }
---
+++ src/app/home/novo-usuario/novo-usuario.service.ts
@@ -0,0 +1,16 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { api } from 'src/app/api/api';
+import { NovoUsuario } from './novo-usuario';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class NovoUsuarioService {
+  constructor(private http: HttpClient) {}
+
+  cadastraNovoUsuario(novoUsuario: NovoUsuario) {
+    return this.http.post(api, novoUsuario);
+  }
+}
